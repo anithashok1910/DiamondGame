be3a6cd19de8a0e0936585c65c79319444ffb376
iambhavin <bhavin_patel666@yahoo.com>
2016-07-29 17:35:39 +0530

    Add files via upload
    Banker class

---
+++ Banker.java
@@ -0,0 +1,28 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by patelbh on 7/29/2016.
+ */
+public class Banker {
+
+    public Banker(Player player1, Player player2, suit)
+    {
+        this.player1 = player1;
+        this.player2 = player2;
+
+    }
+
+
+
+    public int getbid(int firstplayerBid, int secondplayerBid)
+    {
+    if(firstplayerBid > secondplayerBid)
+        return firstplayerBid;
+        else secondplayerBid;
+    }
+
+
+
+}
