69f30a3be3050704f012d1977824136464c256e8
Anith Ashok <anith.ashok1910@gmail.com>
2016-07-29 17:40:23 +0530

    Updated the Player Class with necessary function to deal with.

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,7 @@ import java.util.AbstractCollection;
 import java.util.ArrayList;
 
 /**
- * Created by ashoka on 7/29/2016.
+ * Created by Anith Ashok on 7/29/2016.
  */
 public class Player {
     private ArrayList<Card> playerCardSet;
@@ -10,8 +10,33 @@ public class Player {
     private int currency;
 
     public Player(ArrayList<Card> CardSet){
+        playerCardSet = CardSet;
+        score = 0;
+        for(int i = 0; i<13; i++)
+            currency += playerCardSet.get(i).getPip();
+        currency += 13;
+    }
 
+    public int getScore(){
+        return score;
+    }
 
+    public int getCurrency(){
+        return currency;
+    }
+
+    public void updateScore(int score){
+        this.score += score;
+    }
+
+    public void updateCurrency(int bidAmount) {
+        this.currency -= bidAmount;
+    }
+
+    public int bid(){
+        int cardValue = (int)(Math.random()*playerCardSet.size());
+        playerCardSet.remove(cardValue);
+        return cardValue;
     }
 }
 
